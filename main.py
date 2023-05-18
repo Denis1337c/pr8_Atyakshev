fff99cda147e1dc1425b4b07531ccb783a654646
Denis <zegeenne@mail.ru>
2023-05-19 02:36:08 +0300

    Модернизировал файл mail.py на ветке hotfix

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import ramdom 
+
+chars = 'gfjfsjgjfshgjshqjgnjqhg'
+number = input('количество паролей?'+ "n")
+length = input('длина пароля?'+ "\n")
+number = int(number)
+length = int(length)
+For n in range(number):
+		passowrd = ''
+		for i in range(length):
+			password += random.choice(chers)
+		print(password)
 
